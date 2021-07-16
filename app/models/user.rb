e290c6457e95f1e893438c61232cd4d2fedcf313
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-16 13:54:41 +0900

    新規登録機能の実装(仮)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    #validates :password, format: {with: /\A[a-z0-9]+z\z/i, message: "is invalid. Include both letters and numbers" }     
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :last_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
+    validates :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" } 
+  end
+
+  has_many :items
 end
+
