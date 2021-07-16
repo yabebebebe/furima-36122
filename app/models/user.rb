d24cd89d522ded7b0faa57b65aeac74ac340893a
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-16 14:46:51 +0900

    新規登録機能の実装(修正)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    #validates :password, format: {with: /\A[a-z0-9]+z\z/i, message: "is invalid. Include both letters and numbers" }     
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }     
   with_options presence: true do
     validates :nickname
     validates :birthday
@@ -16,4 +16,3 @@ class User < ApplicationRecord
 
   has_many :items
 end
-
