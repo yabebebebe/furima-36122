a58dde36694c2c8fe033b8f8ab5636633baf37ce
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-19 23:30:11 +0900

    商品出品ページの表示

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveHash::Base
-  self.date = [
+  self.data = [
     { id: 1, name: '--' },
     { id: 2, name: 'メンズ' },
     { id: 3, name: 'レディース' },
--- app/models/scheduled_delivery.rb
+++ app/models/scheduled_delivery.rb
@@ -1,5 +1,5 @@
-class Scheduled_delivery < ActiveHash::Base
-  self.date = [
+class ScheduledDelivery < ActiveHash::Base
+  self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '1~2日で発送' },
     { id: 3, name: '2~3日で発送' },
