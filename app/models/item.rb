547fb03cb6eca8bbbeb3a90527ec1a64eec8e6f3
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-20 01:58:09 +0900

    商品情報のDB保存機能の実装と修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,18 +3,18 @@ class Item < ApplicationRecord
   with_options presence: true do
 
     with_options numericality: { other_than: 1 , message: "can't be blank" } do
+        validates :category_id
+        validates :state_id
+        validates :charge_id
+        validates :prefecture_id
+        validates :scheduled_delivery_id
+    end
+    
       validates :image
       validates :title
       validates :text
-      validates :category_id
-      validates :state_id
-      validates :charge_id
-      validates :scheduled_delivery_id
       validates :price, format: { with: /\A[0-9]+\z/ },
                         numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-    end
-
-    validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank" }
 
   end
 
