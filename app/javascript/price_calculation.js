0de5ea7def9c94108b3f95ecfc3974ab858bdc21
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-20 20:06:57 +0900

    コードレビューの修正

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -3,15 +3,12 @@ addEventListener('load', () => {
   const priceForm = document.getElementById("item-price");
   priceForm.addEventListener("input", () => {
     const formValue = priceForm.value;
-    console.log(formValue);
 
   const priceTax = document.getElementById("add-tax-price");
   priceTax.innerHTML = (Math.floor(formValue * 0.1));
-    console.log(priceTax);
 
   const priceProfit = document.getElementById("profit");
   priceProfit.innerHTML = (Math.floor(formValue - priceTax.innerHTML));
-    console.log(priceProfit);
 
   })
 
