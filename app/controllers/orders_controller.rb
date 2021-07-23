6b95e2a38fcd233a3a91d443bb187bdb48703e8e
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-23 12:03:58 +0900

    購入ページの表示と実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,11 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  #def create  
+  #end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,7 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :user_id, :item_id
 
-  with_options presence: ture do
+  with_options presence: true do
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
